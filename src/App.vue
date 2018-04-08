dd7a61f872b2ba31a197395e879735b43862408b
Adam Bushá <adam@adambusha.com>
2018-04-08 16:44:45 -0400

    Added movie images

--- src/App.vue
+++ src/App.vue
@@ -34,11 +34,10 @@ export default {
 
 <style>
 #app {
+  color: #fff;
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/components/MoviesList.vue
+++ src/components/MoviesList.vue
@@ -1,36 +1,49 @@
 <template>
-  <div>
-    {{ movie.title }}
-  </div>
+  <ul>
+    <li v-for="movie in movies">
+      <Movie :movie="movie" />
+    </li>
+  </ul>
 </template>
 
 <script>
+import Movie from './Movie.vue'
 export default {
   name: 'MoviesList',
-  data() {
+  data () {
     return {
-      movie: {}
-    };
+      movies: []
+    }
   },
-  created: function() {
-    this.fetchData();
+  created: function () {
+    this.fetchData()
   },
   methods: {
-    fetchData: async function() {
+    fetchData: async function () {
       try {
         const res = await fetch(
-          'https://api.themoviedb.org/3/movie/550?api_key=65e043c24785898be00b4abc12fcdaae'
-        );
-        const movie = await res.json();
-        this.movie = movie;
+          'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=65e043c24785898be00b4abc12fcdaae'
+        )
+        const movies = await res.json()
+        this.movies = movies.results
       } catch (e) {
-        console.log(e);
+        console.log(e)
       }
     }
+  },
+  components: {
+    Movie
   }
-};
+}
 </script>
 
 <style scoped>
-
+ul {
+  display: grid;
+  grid-row-gap: 1rem;
+  grid-template-columns: repeat(6, 1fr);
+  list-style: none;
+  margin: 0;
+  padding: 1rem;
+}
 </style>
